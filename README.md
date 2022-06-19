b679c85cf17c9407120d6bde0d09cb62d7a85c57
unknown <note3ook@gmail.com>
2022-06-19 22:30:54 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# PJ Studio Template - Home page / Landing page
+
+## Features & options
+:desktop_computer: Full responsive - Optimized for all screen sizes
+:joystick: Interactive elements
+:keyboard: HTML5, CSS3 & jS
+:heavy_check_mark: Beautiful animation and graphics
+:heavy_check_mark: customisable shapes and elements
+
+##Live demo
+Visit [Demo Page](https://note3ook.github.io/pj-template)
+
+## Full screenshot
+![](assets/images/shot.png)
+
---
+++ assets/script.js
@@ -0,0 +1,52 @@
+'use strict'
+
+let likeButton = document.querySelectorAll('.like-button');
+let upVote = document.querySelectorAll('.pj-up');
+let downVote = document.querySelectorAll('.pj-down');
+let postImages = document.querySelectorAll('.posts-image');
+let imageModalBox = document.querySelector('#image-modal');
+let imageContainer = document.querySelector('#modal-image-container');
+let modalCloseButton = document.querySelector('#modal-close-button');
+
+likeButton.forEach(function (e){
+    e.addEventListener('click', function (){
+        e.classList.toggle('liked');
+    })
+})
+
+upVote.forEach(function (e,i){
+    e.addEventListener('click', function (){
+        if (downVote[i].classList.contains('down-vote')){
+            downVote[i].classList.remove('down-vote');
+        }
+        e.classList.add('up-vote');
+    })
+})
+
+downVote.forEach(function (e,i){
+    e.addEventListener('click', function (){
+        if (upVote[i].classList.contains('up-vote')){
+            upVote[i].classList.remove('up-vote');
+        }
+        e.classList.add('down-vote');
+    })
+})
+
+postImages.forEach(function (e,i){
+    e.addEventListener('click', function (){
+        imageModalBox.classList.add('show');
+        if (i===0){
+            imageContainer.src = './assets/images/first.jpg'
+        } else if (i===1) {
+            imageContainer.src = './assets/images/second.jpg'
+        } else imageContainer.src = './assets/images/third.jpg'
+    })
+})
+
+modalCloseButton.addEventListener('click', function (){
+    imageModalBox.classList.remove('show');
+})
+
+window.addEventListener('click', function (e){
+  if (e.target===imageModalBox) imageModalBox.classList.remove('show');
+})
